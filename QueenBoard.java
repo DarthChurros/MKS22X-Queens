20ac8e095ea0d035411276fb5796274238d65e84
DarthChurros <tnarayan00@stuy.edu>
2019-02-08 11:27:44 -0500

    wrote untested addQueen

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,15 +2,28 @@ import java.util.ArrayList;
 
 public class QueenBoard {
   private int[][] board;
-  private ArrayList<int[]> queens;
+  private boolean empty;
 
   public QueenBoard(int size) {
     board = new int[size][size];
-    queens = new ArrayList<int[]>();
+    empty = true;
   }
 
   private boolean addQueen(int r, int c) {
-    if (board[r][c] == 0) return false;
+    if (board[r][c] != 0) return false;
+    board[r][c]--;
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        if (r - c == i - j
+        || (r == i) != (c == j)) {
+          if (board[i][j] < 0) {
+            board[i][j]--;
+          } else {
+            board[i][j]++;
+          }
+        }
+      }
+    }
     return true;
   }
 }
